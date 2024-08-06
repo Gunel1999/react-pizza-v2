5eddf4b7abfe45091596b7979fbac5f1fa98535b
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-08-06 17:22:30 +0400

    ls#6 finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
 
 const App = () => {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://66b1fc321ca8ad33d4f602e5.mockapi.io/pizzas')
+      .then(res => res.json())
+      .then(json => setPizzas(json));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />

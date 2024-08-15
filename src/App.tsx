aa2a1b8562c44670ac072c8f70d0dfacc16f4900
Gunel1999 <iskenderova.gunel1999@gmail.com>
2024-08-15 23:54:01 +0400

    ls#27 43:39

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import FullPizza from './pages/FullPizza';
 import MainLayout from './layouts/MainLayout';
+
+const Cart = React.lazy(() => import('./pages/Cart'));
+const FullPizza = React.lazy(() => import('./pages/FullPizza'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="cart"
+          element={
+            <React.Suspense fallback={<div>Загрузка корзины...</div>}>
+              <Cart />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="pizza/:id"
+          element={
+            <React.Suspense fallback={<div>Загрузка...</div>}>
+              <FullPizza />
+            </React.Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <React.Suspense fallback={<div>Загрузка...</div>}>
+              <NotFound />
+            </React.Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
